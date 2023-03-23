3e748bd32330aef2c9aaf55ce1f8d2670c8e5822
Elkhan <74042126+ubbeg2000@users.noreply.github.com>
2023-03-23 22:52:03 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,47 +1,76 @@
 /** EL2208 Praktikum Pemecahan Masalah dengan C 2022/2023
- *   Modul               : 
+ *   Modul               :
  *   Hari dan Tanggal    :
  *   Nama (NIM)          :
  *   Nama File           : main.c
  *   Deskripsi           :
  */
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<assert.h>
-  
-//Struct Node Linked List
-struct Node {
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+// Struct Node Linked List
+struct Node
+{
 	int data;
-	struct Node* next;
+	struct Node *next;
 } Node;
 
-void push(struct Node** head, int new_data)
+void push(struct Node **head, int new_data)
 {
-	struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+	struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
 	new_node->data = new_data;
 	new_node->next = NULL;
-	
-	if (*head == NULL) {
+
+	// Kondisi ketika linked list kosong
+	if (*head == NULL)
+	{
 		*head = new_node;
 		return;
 	}
-	
-	if ((*head)->data > new_data) {
+
+	// Kondisi ketika data baru menggantikan head linked list
+	if ((*head)->data > new_data)
+	{
 		new_node->next = *head;
-		**head = *new_node;
+		*head = new_node;
 		return;
 	}
-	
-	struct Node* cur = *head;
-	while (cur->next != NULL) {
-		if (cur->next->data < new_data) {
+
+	// Kondisi ketika linked list hanya berisi satu elemen
+	// dan data baru harus dimasukin di akhir linked list
+	if ((*head)->data < new_data && (*head)->next == NULL)
+	{
+		(*head)->next = new_node;
+		return;
+	}
+
+	struct Node *cur = *head;
+	while (cur->next != NULL)
+	{
+		if (cur->next->next == NULL)
+		{
+			if (cur->next->data < new_data)
+			{
+				cur->next->next = new_node;
+				return;
+			}
+			else
+			{
+				new_node->next = cur->next;
+				cur->next = new_node;
+				return;
+			}
+		}
+
+		if (cur->next->data < new_data)
+		{
 			cur = cur->next;
-		} else if (cur->next->next == NULL) {
-			cur->next->next = new_node;
-			return;
-		} else {
-			new_node->next = cur->next->next;
+		}
+		else
+		{
+			new_node->next = cur->next;
 			cur->next = new_node;
 			break;
 		}
@@ -50,37 +79,40 @@ void push(struct Node** head, int new_data)
 
 int main()
 {
-	//Deklarasi variabel-variabel
-	struct Node* linked_list = NULL;
-	struct Node* cur = NULL;
+	// Deklarasi variabel-variabel
+	struct Node *linked_list = NULL;
+	struct Node *cur = NULL;
 	int n, v, i;
-	
-	//input linked list pertama
+
+	// input linked list pertama
 	printf("Berapa Kali David Lari Pada Minggu 1: ");
 	scanf("%d", &n);
-	
+
 	printf("Masukkan Jarak Yang Ditempuh Pada Minggu 1(meter): ");
-	for (i = 0; i < n; i++) {
+	for (i = 0; i < n; i++)
+	{
 		scanf("%d", &v);
 		push(&linked_list, v);
 	}
-	
-	//input linked list kedua
+
+	// input linked list kedua
 	printf("Berapa Kali David Lari Pada Minggu 2: ");
 	scanf("%d", &n);
-	
+
 	printf("Masukkan Jarak Yang Ditempuh Pada Minggu 2(meter): ");
-	for (i = 0; i < n; i++) {
+	for (i = 0; i < n; i++)
+	{
 		scanf("%d", &v);
 		push(&linked_list, v);
 	}
-	
-	//input linked list ketiga
+
+	// input linked list ketiga
 	printf("Berapa Kali David Lari Pada Minggu 3: ");
 	scanf("%d", &n);
 
 	printf("Masukkan Jarak Yang Ditempuh Pada Minggu 3(meter): ");
-	for (i = 0; i < n; i++) {
+	for (i = 0; i < n; i++)
+	{
 		scanf("%d", &v);
 		push(&linked_list, v);
 	}
